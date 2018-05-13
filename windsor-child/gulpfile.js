c02375472c60e2c88a00676c12f622fabba732bc
k.den <adkalashnikov@gmail.com>
2018-05-13 13:44:57 +0300

    update gulp

--- windsor-child/gulpfile.js
+++ windsor-child/gulpfile.js
@@ -5,26 +5,35 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 const paths = {
-    styles: [
-        '*.scss',
-        'css/*.scss'
-    ]
+    styles: {
+        scss: [
+            '*.scss',
+            'css/*.scss'
+        ],
+        src: '*.scss',
+        dest: './'
+    }
 };
 
-gulp.task('css', function() {
-    return gulp.src('*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest(''))
-        ;
-});
+        .pipe(gulp.dest(paths.styles.dest));
+}
 
-gulp.task('watch', function() {
-    gulp.watch(paths.styles, ['css']);
-});
+function watch() {
+    gulp.watch(paths.styles.scss, styles);
+}
 
-gulp.task('build', ['css']);
-gulp.task('default', ['build']);
+exports.styles = styles;
+exports.watch = watch;
+
+const build = gulp.series(styles);
+
+gulp.task('watch', watch);
+gulp.task('build', build);
+gulp.task('default', build);
